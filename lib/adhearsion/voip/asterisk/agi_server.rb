a4eab493f1ecfce360e8557cd58b00714c919b3e
Jeremy Lawler <jeremylawler@gmail.com>
2009-04-20 21:55:11 -0500

    added a (broken) hook for the server that forks

--- lib/adhearsion/voip/asterisk/agi_server.rb
+++ lib/adhearsion/voip/asterisk/agi_server.rb
@@ -1,4 +1,5 @@
 require 'gserver'
+require 'adhearsion/voip/asterisk/fork_server'
 module Adhearsion
   module VoIP
     module Asterisk
@@ -52,8 +53,55 @@ module Adhearsion
             end
             
           end
-         
-          DEFAULT_OPTIONS = { :server_class => RubyServer, :port => 4573, :host => "0.0.0.0" } unless defined? DEFAULT_OPTIONS
+ 
+         class RubyServer < ForkServer
+            def initialize(port, host)
+              super(port, host, (1.0/0.0)) # (1.0/0.0) == Infinity
+            end
+            
+            def serve(io)
+              call = Adhearsion.receive_call_from(io)
+              Events.trigger_immediately([:asterisk, :before_call], call)
+          	  ahn_log.agi "Handling call with variables #{call.variables.inspect}"
+          	  
+          	  return DialPlan::ConfirmationManager.handle(call) if DialPlan::ConfirmationManager.confirmation_call?(call)
+          	  
+      	      # This is what happens 99.9% of the time.
+      	      
+      	      DialPlan::Manager.handle call
+            rescue DialPlan::Manager::NoContextError => e
+              ahn_log.agi e.message
+              call.hangup!
+            rescue FailedExtensionCallException => failed_call
+              begin
+                ahn_log.agi "Received \"failed\" meta-call with :failed_reason => #{failed_call.call.failed_reason.inspect}. Executing Executing /asterisk/failed_call event callbacks."
+                Events.trigger [:asterisk, :failed_call], failed_call.call
+                call.hangup!
+              rescue => e
+                ahn_log.agi.error e
+              end
+            rescue HungupExtensionCallException => hungup_call
+              begin
+                ahn_log.agi "Received \"h\" meta-call. Executing /asterisk/hungup_call event callbacks."
+                Events.trigger [:asterisk, :hungup_call], hungup_call.call
+                call.hangup!
+              rescue => e
+                ahn_log.agi.error e
+              end
+            rescue UselessCallException
+              ahn_log.agi "Ignoring meta-AGI request"
+              call.hangup!
+        	  # TBD: (may have more hooks than what Jay has defined in hooks.rb)
+            rescue => e
+              ahn_log.agi.error e.inspect
+              ahn_log.agi.error e.backtrace.map { |s| " " * 5 + s }.join("\n")
+            ensure
+              Adhearsion.remove_inactive_call call rescue nil
+            end
+            
+          end
+        
+          DEFAULT_OPTIONS = { :server_class => ForkServer, :port => 4573, :host => "0.0.0.0" } unless defined? DEFAULT_OPTIONS
           attr_reader :host, :port, :server_class, :server
 
           def initialize(options = {})
